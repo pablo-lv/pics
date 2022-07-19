cb796df79444bda8609396cc1b390dc522230031
pablo-lucas <pablo.lucv@gmail.com>
2022-07-18 21:40:51 -0500

    Added controlled input

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+import SearchBar from './SearchBar';
+
+function App() {
+  return (
+    <div className="ui container" style={{marginTop: '10px'}}>
+     <SearchBar/>
+    </div>
+  );
+}
+
+export default App;
