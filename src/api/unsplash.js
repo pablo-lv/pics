84949e159654d466fc0e6d128b0f34534ded0bc9
pablo-lucas <pablo.lucv@gmail.com>
2022-07-22 20:27:59 -0500

    Adde ImageList component and ImageCard

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.unsplash.com",
     headers:{
-        Authorization: "Client-ID "
+        Authorization: "Client-ID bLKnBbNl7LxtBqqSzvaO40IgO4ESnST-NhurKQVC7FQ"
     }
 })
